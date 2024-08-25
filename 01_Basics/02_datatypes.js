1a572e69bf382968b65ce0ccb4282f1b6b2a95cd
Ayush Ray <152051575+Ayushcode24@users.noreply.github.com>
2024-08-25 16:39:22 +0000

    Conversion_Operation

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -2,7 +2,7 @@
 //alert ( 3+9) // we are using nodejs, not browser
 // for reference we can use javascript mdn
  let name = "Ayush" // String datatype
- let age = 22 // Integer datatype higher limit is 2 to power 53
+ let age = 22 // Integer or number datatype higher limit is 2 to power 53
  let isLoggedIn = false // boolean datatype
  let city // undefined datatype
  let state = null // null datatype (Standalone value or empty)
---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,15 @@
+let score = "45A"
+console.log(typeof score)
+console.log(typeof (score))
+let ValueInNumber = Number(score)// conversion
+console.log (typeof ValueInNumber)
+console.log (ValueInNumber)
+// "45" => 45
+// "45A" => NaN  (Not a Number)
+// true => 1 or false => 0
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log (booleanIsLoggedIn)
+// 1 => true; 0=> false
+//"" => false
+//"ayush" => true
