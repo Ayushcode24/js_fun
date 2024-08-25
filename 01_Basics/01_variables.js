2e05ead4160542c913846f96a25dfc445de71aae
Ayush Ray <152051575+Ayushcode24@users.noreply.github.com>
2024-08-25 12:15:05 +0000

    learn variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 123456789
+let accountEmail = "Ak@gmail.com"
+var accountPassword = "Ayush@789"
+accountCity = "Kolkata"
+/* Prefer not to use var because of the scope 
+*/
+console.log(accountId);
+console.table ([accountEmail,accountPassword,accountCity])
+
