eeafbd65cf27b41a0976507f961a088548a0c136
Ayush Ray <152051575+Ayushcode24@users.noreply.github.com>
2024-08-25 16:18:17 +0000

    Types of DataTypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all JS Code as newer version
+//alert ( 3+9) // we are using nodejs, not browser
+// for reference we can use javascript mdn
+ let name = "Ayush" // String datatype
+ let age = 22 // Integer datatype higher limit is 2 to power 53
+ let isLoggedIn = false // boolean datatype
+ let city // undefined datatype
+ let state = null // null datatype (Standalone value or empty)
+ // null is object datatype*
+ // symbol datatypes 
+ console.log(typeof age)// this provide datatype 
