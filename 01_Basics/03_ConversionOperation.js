f785b8615f72a96b5b71c96951d9b7a4a1999c7e
Ayush Ray <152051575+Ayushcode24@users.noreply.github.com>
2024-08-25 18:36:31 +0000

    Operations

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -13,3 +13,33 @@ console.log (booleanIsLoggedIn)
 // 1 => true; 0=> false
 //"" => false
 //"ayush" => true
+
+
+
+//***********************OPERATIONS*******************
+let value = 24
+let negValue = -value
+console.log(negValue)
+
+/*
+console.log(6+9)
+console.log(8-4)
+console.log(8*7)
+console.log(2**4)//power
+console.log(9/3)
+console.log(9%4)// remainder
+*/
+let str1= "Namaste"
+let str2 = " Bharat"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+ 2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(+true)// give output 1
+let counter = 124
+counter++// postfix please read prefix and postfix 
+console.log(counter)
