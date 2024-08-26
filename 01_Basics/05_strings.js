34724fd1f996cd80153437282e607a4244e4801d
Ayush Ray <152051575+Ayushcode24@users.noreply.github.com>
2024-08-26 19:16:25 +0000

    strings and different functions

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "ayush"
+const repoCount = 11
+
+// console.log(name + repoCount + "value" )
+
+console.log(`Hello My Name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('Discord')
+
+console.log(gameName[3]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('r'));
+
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-5,5)
+console.log(anotherString);
+
+
+const newStringOne = "   Namaste "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+
+
