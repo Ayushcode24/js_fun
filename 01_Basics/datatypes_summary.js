5e8a6d25352cc1142ae19f98c2970ab994a8f3c8
Ayush Ray <152051575+Ayushcode24@users.noreply.github.com>
2024-08-26 10:25:37 +0000

    Summary of datatypes

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,27 @@
+// Primitive Datatypes
+ // 7 Types : String,Number,Boolean,Null,undefined,Symbol,BigInt
+
+ // javascript is dynamic language
+
+ // Reference (Non Primitive) Datatypes
+ // Arrays, Objects, Functions
+
+ /*const heros = ["Shaktiman","Ram","Hanuman"];
+ let myObj = {
+    name : "Ayush";
+    age : 22
+ }
+ const myFunction = function(){
+    console.log("Hello bharat");
+    
+ }
+
+
+ // The datatype by using typeof of null is object.
+ // The datatypes by using typeof of non primitive is object.
+ // The datatypes of function by using typeof it return function i.e, function object
+
+ for references we can read  by this link 
+  https://262.ecma-international.org/5.1/#sec-11.4.3
+
+*/
