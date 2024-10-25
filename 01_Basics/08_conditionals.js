683b992c44aa67ffb0fa8b9e2b1163789428ac13
Ayush Ray <152051575+Ayushcode24@users.noreply.github.com>
2024-10-25 04:17:52 +0000

    conditionals

---
+++ 01_Basics/08_conditionals.js
@@ -0,0 +1,23 @@
+// check if a variable is number or not
+let num = 89
+if ( typeof num === 'number'){// number n is small
+    console.log("This is Number.")
+}else{
+    console.log("This is NOT a Number")
+}
+// check if boolean value is true or false.
+let isTeaReady = true;
+if(isTeaReady){
+    console.log("Tea is ready.")
+}else{
+    console.log("Tea is NOT Ready.")
+}
+
+// checking if array is empty or not :
+let items = []
+
+if(items.length == 0){
+    console.log("Array is empty.")
+}else{
+    console.log("Array is NOT empty.")
+}
